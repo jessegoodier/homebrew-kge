a21c4846a935b780f471858a17f8452f09e1c9bc
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2025-04-19 18:23:00 -0400

    0.7.0 Signed-off-by: jesse goodier <31039225+jessegoodier@users.noreply.github.com>

--- Formula/kge.rb
+++ Formula/kge.rb
@@ -7,16 +7,21 @@ class Kge < Formula
   sha256 "4b8d47cb0e1d55eadc06c6f307b8774f4ea25dcff794569527ba42b644a24b85"
   license "MIT"
 
-  depends_on "python@3"
+  depends_on "python@3.11" => :recommended
 
-  # Add dependencies
+
+
   resource "colorama" do
-    url "https://pypi.org/simple/#colorama/"
+    url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
+    sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
   end
+
 
   resource "kubernetes" do
-    url "https://pypi.org/simple/#kubernetes/"
+    url "https://pypi.org/packages/source/k/kubernetes/kubernetes-12.0.0.tar.gz"
+    sha256 "72f095a1cd593401ff26b3b8d71749340394ca6d8413770ea28ce18efd5bcf4c"
   end
+\
 
   def install
     virtualenv_install_with_resources
